65b230ea1579f15982cdb00b0ebf96383d03f484
@yenldh <yenldh>
2021-03-13 23:12:41 +0700

    Updates

---
+++ precache-manifest.fde672c52010ce912950c3053f8320a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878d7d857d6b7e8622b82599deab7076",
+    "url": "/huong-viet-resto/index.html"
+  },
+  {
+    "revision": "199e7934dd58870a0fe2",
+    "url": "/huong-viet-resto/static/css/main.ce57a660.chunk.css"
+  },
+  {
+    "revision": "a6ced30759986021221c",
+    "url": "/huong-viet-resto/static/js/2.ecddc24e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/huong-viet-resto/static/js/2.ecddc24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add6ebd3fbbc191beee5",
+    "url": "/huong-viet-resto/static/js/3.3e5e03a7.chunk.js"
+  },
+  {
+    "revision": "048a42b0d0f3d1faffdf",
+    "url": "/huong-viet-resto/static/js/4.36b79e54.chunk.js"
+  },
+  {
+    "revision": "199e7934dd58870a0fe2",
+    "url": "/huong-viet-resto/static/js/main.5a6a7975.chunk.js"
+  },
+  {
+    "revision": "3b7fce0bce1a250dcb20",
+    "url": "/huong-viet-resto/static/js/runtime-main.86f3fe7b.js"
+  },
+  {
+    "revision": "c4f2d6a3cdae4be0799cc9773be1b861",
+    "url": "/huong-viet-resto/static/media/slide01.c4f2d6a3.jpg"
+  },
+  {
+    "revision": "a834c25eb08c49495f468b0e0eb8d62c",
+    "url": "/huong-viet-resto/static/media/slide02.a834c25e.jpg"
+  },
+  {
+    "revision": "6108a8f7162694c793fe058c414cd9e7",
+    "url": "/huong-viet-resto/static/media/slider03.6108a8f7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HuongVietRestaurant/precache-manifest.82b9c52308d1ea19dbcbef2f05154151.js"
+  "/huong-viet-resto/precache-manifest.fde672c52010ce912950c3053f8320a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HuongVietRestaurant/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/huong-viet-resto/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
